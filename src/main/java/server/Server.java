48e5234ed664e20c5e9b092f066f29cb6e5043d3
Dominik Kipfer <dominik.kipfer@stud.unibas.ch>
2021-03-21 16:43:45 +0100

    Update server13

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -22,7 +22,7 @@ public class Server {
                 thread.start(); // Noncomplthjjhftz
             }
         } catch (IOException e) { //wedewdewedwed
-            e.printStackTrace(); //wqdwdtzfufuzugwefewfwefeefwefefwas
+            e.printStackTrace(); //wqdwdtzfufuzugwefewfwefeefwetfztfefwas
         }
     }
 
