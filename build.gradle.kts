bd25b2fdac240a5a476c0e2ac02e12e76ecc5331
Domonkos <162434141+domonkosszer@users.noreply.github.com>
2025-02-04 15:43:24 +0100

    build.gradle.kts updated JSONObject usable

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.json:json:20231013")
+    implementation("org.json:json:20230227")
+
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
