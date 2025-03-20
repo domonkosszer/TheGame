6a453bd40b663d6824f903868f5c1597346522cc
Dominik Kipfer <dominik.kipfer@stud.unibas.ch>
2025-03-20 12:21:45 +0100

    Update server3

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -8,7 +8,7 @@ public class Server {
     private final ServerSocket serverSocket;
 
     public Server(ServerSocket serverSocket) {
-        this.serverSocket = serverSocket; // Noncompl
+        this.serverSocket = serverSocket; // Noncompltftz
     }
 
     public void startServer() {
